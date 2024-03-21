e692d345c70cdb526ee90e43b626ff959571393d
Klaus <nick@koeppen.net>
2024-03-21 16:37:58 -0700

    Genres Table Added

--- library/webapp/forms.py
+++ library/webapp/forms.py
@@ -5,7 +5,7 @@ from .models import Author
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'isbn', 'publisher_id', 'publication_date']
+        fields = ['title', 'author', 'isbn', 'publisher_id', 'publication_date', 'genres']
 
 class AuthorForm(forms.ModelForm):
     class Meta:
--- library/webapp/models.py
+++ library/webapp/models.py
@@ -9,6 +9,13 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
     
 class Book(models.Model):
     title = models.CharField(max_length=255)
@@ -16,9 +23,13 @@ class Book(models.Model):
     isbn = models.CharField(max_length=20)
     publisher_id = models.IntegerField()  # Consider updating this similarly in the future
     publication_date = models.DateField()
+    genres = models.ManyToManyField(Genre, related_name='books')
 
     def __str__(self):
         return self.title
+    
+
+
 
 
 
